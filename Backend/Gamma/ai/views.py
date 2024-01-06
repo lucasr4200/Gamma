0a9ee3d12a5737862ce4483e474b51c0afcfe2d2
Cameron <cameron.matthew101@gmail.com>
2024-01-06 12:57:34 -0700

    changes

--- Backend/Gamma/ai/views.py
+++ Backend/Gamma/ai/views.py
@@ -1,22 +1,24 @@
 from rest_framework.views import APIView
 import json
-import requests
+from rest_framework.response import Response
 from openai import OpenAI
 import os
 from __init__ import client
 
 # Create your views here.
 
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Say this is a test",
-        }
-    ],
-    model="gpt-3.5-turbo",
-)
-
-
 class ChatView(APIView):
-    
+
+    def post(self, request):
+        data = json.loads(request.body)
+        message = data['message']
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": message,
+                }
+            ],
+            model="gpt-3.5-turbo",
+        )
+        return Response(chat_completion)
