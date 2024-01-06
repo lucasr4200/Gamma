f449cb085a48e7dca438a87a8f7ebd8d798db68f
Lucas Rasmusson <lucasr4200@gmail.com>
2024-01-06 13:55:22 -0700

    Local host bot
    Bot captures message, user, timestamp and channel in which the message was sent

--- Discord/main.py
+++ Discord/main.py
@@ -1,4 +1,6 @@
 import discord
+import os
+import requests
 
 client = discord.Client(intents=discord.Intents.all())
 
@@ -11,6 +13,16 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    data = {'message': message.content,
+            'user': str(message.author),
+            'timestamp': str(message.created_at),
+            'channel': str(message.channel)}
+    response = requests.post('http://localhost:8000/ai/', data=data)
+
+
+
     await message.channel.send(message.content)
 
-client.run('your-bot-token')
+
+token = os.environ.get('DISCORD_TOKEN')
+client.run(token)
